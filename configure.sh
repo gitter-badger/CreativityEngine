33eab99ab069e3d83281060c29fd119ec81f21f0
Sean McElholm <seanmcelholm@gmail.com>
2017-01-13 19:54:10 +0000

    Add g++ to travis

--- configure.sh
+++ configure.sh
@@ -4,6 +4,7 @@ if [[ "$unamestr" == 'Linux' ]]; then
     export DEBIAN_FRONTEND=noninteractive
     sudo apt-get update
     # sudo apt-get -q -y upgrade
+    sudo apt-get -q -y install g++
     sudo apt-get -q -y install build-essential
     sudo apt-get -q -y install freeglut3-dev 
     sudo apt-get -q -y install libsdl2-dev
