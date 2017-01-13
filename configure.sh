c516c7f75835881eadc5cfbf40711693ed984433
Sean McElholm <seanmcelholm@gmail.com>
2017-01-13 19:30:15 +0000

    Attempt fix linux travis failing builds

--- configure.sh
+++ configure.sh
@@ -1,11 +1,14 @@
 platform='unknown'
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
-   sudo apt-get update
-   sudo apt-get install --yes libsdl2-dev
+    sudo apt-get update
+    sudo apt-get upgrade
+    sudo apt-get install --yes build-essential
+    sudo apt-get install --yes freeglut3-dev 
+    sudo apt-get install --yes libsdl2-dev
 elif [[ "$unamestr" == 'Darwin' ]]; then
-   brew install sdl2
+    brew update
+    brew install sdl2
 fi
 
-
-mkdir bin
+mkdir -p bin
