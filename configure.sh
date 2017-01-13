19df30a7274da2ff05b8728f17f68e06a29da371
Sean McElholm <seanmcelholm@gmail.com>
2017-01-13 19:51:34 +0000

    Jesus, Travis-CI is hard to setup :(

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@ unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
     export DEBIAN_FRONTEND=noninteractive
     sudo apt-get update
-    sudo apt-get -q -y upgrade
+    # sudo apt-get -q -y upgrade
     sudo apt-get -q -y install build-essential
     sudo apt-get -q -y install freeglut3-dev 
     sudo apt-get -q -y install libsdl2-dev
